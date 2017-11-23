5c7052fa04c55536af9ad7b213b0b312255937b8
Danny Leshem <dleshem@gmail.com>
2017-11-23 16:07:59 +0200

    Added WIX_RESTAURANTS_RESERVATIONS

--- wix-restaurants-java-client/src/main/java/com/wix/restaurants/WixAppIds.java
+++ wix-restaurants-java-client/src/main/java/com/wix/restaurants/WixAppIds.java
@@ -6,4 +6,5 @@ public class WixAppIds {
     public static final String WIX_RESTAURANTS_MENUS = "13c1402c-27f2-d4ab-7463-ee7c89e07578";
     public static final String WIX_RESTAURANTS_ORDERS = "13e8d036-5516-6104-b456-c8466db39542";
     public static final String WIX_RESTAURANTS_KIT = "14583ff5-e781-063a-3bc4-6b79fb966992";
+    public static final String WIX_RESTAURANTS_RESERVATIONS = "1475ab65-206b-d79a-856d-fa10bdb479ea";
 }
