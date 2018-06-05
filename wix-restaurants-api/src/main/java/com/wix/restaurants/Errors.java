f99ccbdb6d09117b3237b2809444a5e42fd6d309
Danny Leshem <dleshem@gmail.com>
2018-06-05 15:54:54 +0300

    Added Errors.TemporarilyUnavailable

--- wix-restaurants-api/src/main/java/com/wix/restaurants/Errors.java
+++ wix-restaurants-api/src/main/java/com/wix/restaurants/Errors.java
@@ -3,15 +3,20 @@ package com.wix.restaurants;
 public class Errors {
     private Errors() {}
 
-    public static final String Deprecated      = "https://www.wixrestaurants.com/errors/deprecated";
-    public static final String Internal        = "https://www.wixrestaurants.com/errors/internal";
-    public static final String PaymentRejected = "https://www.wixrestaurants.com/errors/cc_rejected";
-    public static final String InvalidData     = "https://www.wixrestaurants.com/errors/invalid_data";
-    public static final String Forbidden       = "https://www.wixrestaurants.com/errors/no_permission";
-    public static final String Conflict        = "https://www.wixrestaurants.com/errors/conflict";
-    public static final String Authentication  = "https://www.wixrestaurants.com/errors/authentication";
-    public static final String NotSecure       = "https://www.wixrestaurants.com/errors/not_secure";
-    public static final String NotFound        = "https://www.wixrestaurants.com/errors/not_found";
+    // Client
+    public static final String InvalidData            = "https://www.wixrestaurants.com/errors/invalid_data";
+    public static final String PaymentRejected        = "https://www.wixrestaurants.com/errors/cc_rejected";
+    public static final String Forbidden              = "https://www.wixrestaurants.com/errors/no_permission";
+    public static final String Conflict               = "https://www.wixrestaurants.com/errors/conflict";
+    public static final String Authentication         = "https://www.wixrestaurants.com/errors/authentication";
+    public static final String NotSecure              = "https://www.wixrestaurants.com/errors/not_secure";
+    public static final String NotFound               = "https://www.wixrestaurants.com/errors/not_found";
+
+    // Server
+    public static final String Internal               = "https://www.wixrestaurants.com/errors/internal";
+    public static final String TemporarilyUnavailable = "https://www.wixrestaurants.com/errors/temporarily_unavailable";
+    public static final String Deprecated             = "https://www.wixrestaurants.com/errors/deprecated";
+
 
     // TODO: consolidate with other error types
     public static final String CannotSubmitOrder        = "https://www.wixrestaurants.com/errors/cannot_submit_order";
